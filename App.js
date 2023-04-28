e8b73054506a8a446161f82f7f7dea1db534feda
Chris Leblanc <chrisleblanc@microsoft.com>
2023-04-28 09:44:19 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,554 @@
+
+/*****
+  This is a React Native app that utilizes the NuanceMix API for speech-to-text and text-to-speech conversion. 
+  The app has three dialog boxes with different styling options, including a custom footer input, footer listener, and progress bar. 
+  The app also includes static text spoken by different voices and languages supported by the NuanceMix API. 
+  The app requests user permissions for recording audio and accessing location data. 
+  The chat bubbles can be customized for the left and right side of the screen, and one option includes rendering HTML text within the bubble.
+*****/
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+
+import React, {Node, useRef} from 'react';
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  Pressable,
+  Text,
+  TextInput,
+  Modal,
+  NativeModules,
+  SafeAreaView,
+} from 'react-native';
+import ProgressBar from 'react-native-animated-progress';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import WebView from 'react-native-webview';
+import RenderHtml from 'react-native-render-html';
+
+import {request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+
+import { NuanceMixInput, NuanceMixText, NuanceMixChat } from 'nuance-mix';
+
+function CustomFooterInput({ value, onSubmitEditing, onChangeText }) {
+  return (
+    <View style={{marginTop:8}}>
+    <View style={{backgroundColor: "black", height:1, width:"90%", alignSelf:"center"}} />
+        <TextInput
+            style={styles.chatInputStyle}
+            value={value}
+            onSubmitEditing={onSubmitEditing}
+            onChangeText={(text) => onChangeText(text)}
+        />
+    <View style={{backgroundColor: "black", height:1, width:"90%", alignSelf:"center"}} />  
+    </View>
+  );
+}
+
+function CustomFooterInputTextOnly({ value, onSubmitEditing, onChangeText }) {
+  const inputRef = useRef();
+
+  return (
+    <View style={{marginTop:8}}>
+    <View style={{backgroundColor: "black", height:1, width:"90%", alignSelf:"center"}} />
+        <TextInput
+            style={styles.chatInputStyle}
+            value={value}
+            onSubmitEditing={onSubmitEditing}
+            onChangeText={(text) => onChangeText(text)}
+            ref={inputRef}
+            onLayout={() => inputRef.current.focus()}
+        />
+    <View style={{backgroundColor: "black", height:1, width:"90%", alignSelf:"center"}} />  
+    </View>
+  );
+}
+
+function CustomFooterListener({ onPress }) {
+  return (
+      <Pressable 
+        style={styles.button}
+        onPress={onPress}>
+          <Text style={styles.buttontext}>Listen</Text>
+      </Pressable>
+  );
+}
+
+function CustomFooterProgress({ isAnimating }) {
+  return (
+    <View style={{marginTop:3}}>
+      <ProgressBar height={7} animated={false} indeterminate={isAnimating} trackColor='white' backgroundColor="#44AA00"/>
+    </View>
+  );
+}
+
+function CustomLeftBubble( { key, text } ) {
+  return (
+    <View style={styles.chatLeft} key={key}>
+      <Text style={{ fontWeight: 'bold', fontSize: 12, color: "#000",justifyContent:"center" }} key={key}>{text}</Text>
+    </View>
+
+  );
+}
+
+function CustomLeftBubbleHtml( { key, text } ) {
+  return (
+    <View style={styles.chatLeftHtmlView}>
+      <RenderHtml key={key} 
+        contentWidth={200}
+        style={styles.chatLeftHtml}
+        source={{html: text}}
+        startInLoadingState={true}
+      />
+    </View>
+  );
+}
+
+function CustomRightBubble( { key, text } ) {
+  return (
+    <View style={styles.chatRight} key={key}>
+      <Text style={{ fontWeight: 'bold',  fontSize: 12, color: "#fff",justifyContent:"center" }} key={key}>{text}</Text>
+    </View>
+
+  );
+}
+
+const App: () => Node = () => {
+  const [modalOneVisible, setModalOneVisible] = React.useState(false);
+  const [modalTwoVisible, setModalTwoVisible] = React.useState(false);
+  const [modalThreeVisible, setModalThreeVisible] = React.useState(false);
+  const NuanceMix = NativeModules.NuanceMix;
+
+  request(PERMISSIONS.ANDROID.RECORD_AUDIO).then((result) => {
+    // …
+  });
+  request(PERMISSIONS.IOS.MICROPHONE).then((result) => {
+    // …
+  });
+  request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then((result) => {
+    // …
+  });
+  request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE).then((result) => {
+    // …
+  });
+
+  return (
+    <SafeAreaView style={{marginTop:30}}>             
+      <NuanceMixText
+        style={styles.staticText}
+        viewStyle={styles.staticContainer}
+        voice="Evan"
+      >
+        Here is some static text using Evan speaking in english.
+      </NuanceMixText>   
+      <NuanceMixText
+        style={styles.staticText}
+        viewStyle={styles.staticContainer}
+        voice="Paulina-Ml"
+        language="es-MX"
+        ssml="<speak>Aquí está Paulina hablando español.</speak>"
+      >
+        Here is Paulina speaking Spanish.
+      </NuanceMixText>   
+      <NuanceMixInput
+        viewStyle={styles.inputContainer}
+        style={styles.inputStyle}
+        multiline={true}
+        component={<Icon
+                      style={styles.micon}
+                      name="microphone"
+                      size={20}
+                      color="#440044"
+                    />
+                  }
+      />
+      <NuanceMixInput
+        viewStyle={styles.inputContainer}
+        style={styles.inputStyle}
+        multiline={true}
+        language="fra-CAN"
+        component={<Pressable 
+                      style={styles.button}
+                    >
+                      <Text style={styles.buttontext}>En français</Text>
+                    </Pressable>
+                  }
+      />
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalOneVisible}
+        presentationStyle='overFullScreen'
+        onRequestClose={() => {
+          setModalOneVisible(!modalOneVisible);
+          NuanceMix.stopDialog();
+        }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Icon
+                style={styles.xicon}
+                name="close"
+                size={20}
+                color="#440044"
+                onPress={() => {
+                  setModalOneVisible(!modalOneVisible);
+                  NuanceMix.stopDialog();
+                }}
+              />
+              <NuanceMixChat 
+                  viewStyle={styles.chatContainer}
+                  footerStyle={styles.chatInputStyle}
+              />
+            </View>
+          </View>
+      </Modal>
+    </View>
+    <Pressable
+        style={styles.button}
+        onPress={() => setModalOneVisible(true)}>
+        <Text style={styles.buttontext}>Dialog Styling Default</Text>
+      </Pressable>
+
+      <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalTwoVisible}
+        presentationStyle='overFullScreen'
+        onRequestClose={() => {
+          setModalTwoVisible(!modalTwoVisible);
+          NuanceMix.stopDialog();
+        }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+            <Icon
+                style={styles.xicon}
+                name="close"
+                size={20}
+                color="#440044"
+                onPress={() => {
+                  setModalTwoVisible(!modalTwoVisible);
+                  NuanceMix.stopDialog();
+                }}
+              />
+              <NuanceMixChat 
+                  viewStyle={styles.chatContainer}
+                  contextTag="OmniChris"
+                  footerStyle={styles.chatInputStyle}
+                  FooterListener={CustomFooterListener}
+                  FooterProgress={CustomFooterProgress}
+                  LeftBubble={CustomLeftBubbleHtml}
+                  RightBubble={CustomRightBubble}
+                  FooterInput={CustomFooterInput}
+              />
+            </View>
+          </View>
+      </Modal>
+    </View>
+    <Pressable
+        style={styles.button}
+        onPress={() => setModalTwoVisible(true)}>
+        <Text style={styles.buttontext}>Dialog Styling Custom</Text>
+      </Pressable>
+
+      <View  style={styles.centeredView}>
+      <Modal
+        avoidKeyboard
+        animationType="slide"
+        transparent={true}
+        visible={modalThreeVisible}
+        presentationStyle='overFullScreen'
+        onRequestClose={() => {
+          setModalThreeVisible(!modalThreeVisible);
+          NuanceMix.stopDialog();
+        }}>
+          <KeyboardAvoidingView behavior='height' style={styles.keyboardView}>
+            <View style={styles.modalView}>
+              <Icon
+                style={styles.xicon}
+                name="close"
+                size={20}
+                color="#440044"
+                onPress={() => {
+                  setModalThreeVisible(!modalThreeVisible);
+                  NuanceMix.stopDialog();
+                }}
+              />
+              <NuanceMixChat 
+                  viewStyle={styles.chatContainer}
+                  contextTag="TravelChris"
+                  footerStyle={styles.chatInputStyle}
+                  FooterInput={CustomFooterInputTextOnly}
+                  FooterListener={() => {return(<View/>)}}
+                  LeftBubble={CustomLeftBubble}
+                  RightBubble={CustomRightBubble}
+              />
+            </View>
+          </KeyboardAvoidingView>
+      </Modal>
+    </View>
+    <Pressable
+        style={styles.button}
+        onPress={() => setModalThreeVisible(true)}>
+        <Text style={styles.buttontext}>Dialog Styling Text Only</Text>
+      </Pressable>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  keyboardView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  chatLeftHtmlView: {
+    flex: 1,
+    flexDirection: 'column',
+    borderWidth: 1,
+    padding:10,
+    marginTop: 5,
+    marginLeft: "5%",
+    maxWidth: '70%',
+    width: '70%',
+    height: '100%',
+    alignSelf: 'flex-start',
+    borderRadius: 6,
+  },
+chatLeftHtml: {
+  padding:10,
+  alignSelf: 'flex-start',
+  borderRadius: 6,
+},
+chatLeft: {
+  backgroundColor: "#aaaaaa",
+  padding:10,
+  marginTop: 5,
+  marginLeft: "5%",
+  maxWidth: '70%',
+  alignSelf: 'flex-start',
+  borderRadius: 6,
+},
+chatRight: {
+    backgroundColor: "#222222",
+    padding:10,
+    marginTop: 5,
+    marginRight: "5%",
+    maxWidth: '70%',
+    alignSelf: 'flex-end',
+    borderRadius: 6,
+},
+centeredView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  modalView: {
+    margin: 8,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 3,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width:'90%',
+    height:'90%'
+  },
+  modalViewTextOnly: {
+    margin: 8,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 3,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width:'90%',
+    height:'90%'
+  },
+  chatButton: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  chatButtonOpen: {
+    backgroundColor: '#336600',
+  },
+  chatButtonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },  
+  chatIcon: {
+    marginTop:2,
+    marginBottom:2,
+    marginLeft: "50%",
+  },
+  micon: {
+    position: "absolute",
+    top: '50%',
+    right: 8,
+  },
+  xicon: {
+    position: "absolute",
+    top: '1%',
+    right: 8,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 4,
+    paddingHorizontal: 18,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#336600',
+    alignSelf: 'center',
+  },
+  buttontext: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  chatContainer: {
+    marginTop: 20,
+    height: "95%",
+    width: '90%',
+    marginLeft: 5,
+    marginRight: 5,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+//    backgroundColor:"#00ff55",
+  },
+  staticContainer: {
+    marginTop: 10,
+    paddingHorizontal: 24,
+    marginBottom: 10,
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginLeft: 5,
+    marginRight: 5,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#003300",
+  },
+  staticText: {
+    marginTop: 10,
+    marginBottom: 10,
+    paddingHorizontal: 24,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: "white",
+  },
+  inputContainer: {
+    marginTop: 20,
+    paddingHorizontal: 24,
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  inputStyle: {
+    marginTop: 20,
+    paddingHorizontal: 24,
+    fontSize: 14,
+    fontWeight: '600',
+    borderWidth: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  chatInputStyle: {
+    marginTop: 4,
+    marginBottom: 4,
+    paddingHorizontal: 24,
+    fontSize: 16,
+    marginLeft: 5,
+    marginRight: 5,
+    borderWidth: 1,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    color:"black"
+  },
+  rightArrow: {
+    position: "absolute",
+    backgroundColor: "#0078fe",
+    //backgroundColor:"red",
+    width: 20,
+    height: 25,
+    bottom: 0,
+    borderBottomLeftRadius: 25,
+    right: -10
+  },
+  
+  rightArrowOverlap: {
+    position: "absolute",
+    backgroundColor: "#eeeeee",
+    //backgroundColor:"green",
+    width: 20,
+    height: 35,
+    bottom: -6,
+    borderBottomLeftRadius: 18,
+    right: -20
+  
+  },
+  
+  /*Arrow head for recevied messages*/
+  leftArrow: {
+      position: "absolute",
+      backgroundColor: "#dedede",
+      //backgroundColor:"red",
+      width: 20,
+      height: 25,
+      bottom: 0,
+      borderBottomRightRadius: 25,
+      left: -10
+  },
+  
+  leftArrowOverlap: {
+      position: "absolute",
+      backgroundColor: "#eeeeee",
+      //backgroundColor:"green",
+      width: 20,
+      height: 35,
+      bottom: -6,
+      borderBottomRightRadius: 18,
+      left: -20
+  
+  },});
+
+export default App;
--- README.md
+++ README.md
@@ -1,2 +1,178 @@
-# NuanceMixReactNativeDemo
-Demo project for the react-native-nuance-mix NPM plugin for use with git hub codespaces
+# mix-react-native-mobile
+React Native mobile NPM and demonstration mobile app
+
+# Use google codespaces to develop for iOS and Android with the AwesomeProject
+Just open the project with codespaces and edit the config.json file in your android/app/src/main/assets folder as well as ios/config.json to contain the client_id and client_secret you get when logging into mix.nuance.com
+
+You'll also want to create a login at expo.dev and download the expo app to your handset. Don't worry the free account is all you need.
+
+Once you've done that you need a terminal where you can login to expo and run `eas build` as shown below. It will provide a QR code that allows you to install the dev client on your device. The next step is to open another terminal window and run `./AwesomeProject/node_modules/.bin/ngrok http 3000` to provide a tunnel through which you will load you javascript bundle during development. You only need to run the `eas build` step if you change the underlying native code. 
+
+setting up expo is easy 
+https://youtu.be/id0Im72UN6w
+
+
+## Check expo build queue and process here [ free tier 8-/ ]
+https://docs.expo.dev/build-reference/troubleshooting/
+
+```
+@cleblanc-nuance ➜ /workspaces/mix-react-native-mobile (main) $ expo login
+WARNING: The legacy expo-cli does not support Node +17. Migrate to the versioned Expo CLI (npx expo).
+✔ Username/Email Address: … christopher.j.leblanc@gmail.com
+✔ Password: … ***********
+
+@cleblanc-nuance ➜ /workspaces/mix-react-native-mobile/AwesomeProject (main) $ eas build --profile development
+
+Success. You are now logged in as cleblanc189.
+✔ Select platform › All
+Specified value for "android.package" in app.json is ignored because an android directory was detected in the project.
+EAS Build will use the value found in the native code.
+
+🤖 Android build
+✔ Using remote Android credentials (Expo server)
+✔ Using Keystore from configuration: Build Credentials 9e78kLJqNr (default)
+
+Compressing project files and uploading to EAS Build. Learn more
+✔ Compressed project files 4s (18.7 MB)
+✔ Uploaded to EAS 
+Specified value for "ios.bundleIdentifier" in app.json is ignored because an ios directory was detected in the project.
+EAS Build will use the value found in the native code.
+
+🍎 iOS build
+✔ Using remote iOS credentials (Expo server)
+
+If you provide your Apple account credentials we will be able to generate all necessary build credentials and fully validate them.
+This is optional, but without Apple account access you will need to provide all the missing values manually and we can only run minimal validation on them.
+✔ Do you want to log in to your Apple account? … yes
+
+› Log in to your Apple Developer account to continue
+✔ Apple ID: … christopher.j.leblanc@gmail.com
+› Restoring session /home/node/.app-store/auth/christopher.j.leblanc@gmail.com/cookie
+› Team Chris LeBlanc (C8UGF323HP)
+› Provider Chris LeBlanc (126232895)
+✔ Switched to provider: Chris LeBlanc (126232895)
+✔ Logged in Local session
+✔ Bundle identifier registered com.nuance.AwesomeProject
+✔ Synced capabilities: No updates
+✔ Synced capability identifiers: No updates
+✔ Fetched Apple distribution certificates
+✔ Fetched Apple provisioning profiles
+
+Project Credentials Configuration
+
+Project                   @cleblanc189/AwesomeProject
+Bundle Identifier         com.nuance.AwesomeProject
+                          
+App Store Configuration   
+                          
+Distribution Certificate  
+Serial Number             7E65E190EFF944F53800823408821491
+Expiration Date           Sat, 06 Apr 2024 19:31:51 UTC
+Apple Team                C8UGF323HP (Chris LeBlanc (Individual))
+Updated                   11 days ago
+                          
+Provisioning Profile      
+Developer Portal ID       2ZDK5A2RWX
+Status                    active
+Expiration                Sat, 06 Apr 2024 19:31:51 UTC
+Apple Team                C8UGF323HP (Chris LeBlanc (Individual))
+Updated                   11 days ago
+                          
+All credentials are ready to build @cleblanc189/AwesomeProject (com.nuance.AwesomeProject)
+
+
+Compressing project files and uploading to EAS Build. Learn more
+✔ Compressed project files 2s (18.7 MB)
+✔ Uploaded to EAS 
+
+🤖 Android build details: https://expo.dev/accounts/cleblanc189/projects/AwesomeProject/builds/8d15b6a9-a843-4aef-a9d3-4c72b810c603
+🍎 iOS build details: https://expo.dev/accounts/cleblanc189/projects/AwesomeProject/builds/76de2e77-1ef9-40e7-8be4-bb66490de3b8
+
+⠙ Waiting for builds to complete. You can press Ctrl+C to exit.
+  🤖 Android build - status: finished
+
+✔ 🤖 Android build - status: finished
+  🍎 iOS build - status: finished
+🤖 Android app:
+https://expo.dev/artifacts/eas/iqsMrcah58gCsVkdcTswC4.aab
+
+🍎 iOS app:
+https://expo.dev/artifacts/eas/fFkRjGAM7mYxQJHZupokAY.ipa
+Waiting for build to complete. You can press Ctrl+C to exit.
+
+  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+  █ ▄▄▄▄▄ █▀▀▀ ▀  ▄█▄▀ ▀█▀  ▀▀▄▄█▄▀█▀▀  ▀▀▀ █ ▄▄▄▄▄ █
+  █ █   █ █▀▄▀▄▀▀ █▄ ▀▀▀▄█▀▀▀▄▄▄▄▀▀▄█▄█▀▀█ ▄█ █   █ █
+  █ █▄▄▄█ █▀ ▀▄▀█▀▄█ ▀ █▄ ▄▄▄  █▀▄█▄▀▀▀ ▄▀███ █▄▄▄█ █
+  █▄▄▄▄▄▄▄█▄█ ▀▄█▄▀▄█ █ █ █▄█ ▀ ▀▄█ █▄▀▄▀▄▀ █▄▄▄▄▄▄▄█
+  █▄  ▄▄█▄ ▄▄██▄▄▀ ▄ ▄▀▄▄▄  ▄▄▀█▄ ▄▀ █▀▄ ██▄▄▀ ▀ █ ██
+  █  ▄▀ ▀▄█ ▄▀▄ ▀█▀▀▄  █ ▄▄██▄█▀ █▀▀█ ▄▄▀  ▄▄▀ ▀▀▄▀ █
+  ██   ▀█▄ █ ▄▀▄▀█▄▀█ ▀▄▄ ▄  ███   ███   ▄▄▄▄▀ █▄▄ ▄█
+  █▀  ▀▄█▄ ▀▀ ██▄█▄ ▀▄▀▀▄▄▄▄▀  ▀██▄▀▄▄▀▄█ ▄█▄▀ ▀▄▄  █
+  █▄▄▄▀▀▀▄ ██ █▀▀▀▀▄█▄▀▄▄  ▄▀▄█▀▀  █ █▀  █▄▄ ▀█▀▄ ▄██
+  █▄▀   █▄█▀▄ ▀ ▄  █▄██ █▄▄█▀▄▀▀██▀▀ ▄▄▄▀ █ ██▄▀██  █
+  █▄▄▄▀▄ ▄ ▀▄▄▀▄▄▄▀█▄▀▀▄▀ ▄▄▄██▀▀  ▀██ ▄█▄█▄█▀ ▀█▄███
+  █▀▀▄  ▄▄▄  █▀▄ ▀▀█ ███▄ ▄▄▄ █ ▄▀█ ▄  █  █ ▄▄▄  █▄ █
+  █ ▀▀█ █▄█ ▀ ▄▄▀▀▀▄▄▄▀▄▄ █▄█ ▄█▄ ▄█▀██▄▀█  █▄█  ▄ ▄█
+  █▄█▄▀▄ ▄▄ ▄▄▀▀▀█▀▀▀   ▀▄ ▄  ▀▀ ▀ ▀█ █▄▀ ▄▄▄▄▄▄█▄▀ █
+  █  ▄▄██▄▀▀▀▄▀▄▀█▄▀▀  ▄▀▄█▀ █▄▀▀ ██▄▄▄  █  █▄▀▄▄  ██
+  █▄▀█▀▄▀▄██▀▄▄▄▀▀▄ ▀ ▀ ▀▀   ▀█ ▄▀ ▀▄▄ █▀▄▄▀▀▄▀▄▀█  █
+  █▄▄▄▀▄ ▄▀███  █▀▀▄▄▄▀▄▀▄█▀▄▀▄██▀▀█▀▄█▄▀█ ▄ █▀▄█▄███
+  ██ █▀█▄▄█ ▀ █ ▄ ▀▀▄██▄▀▄ █▀▀   ▀  ▀ █▄ ▄▄▄█▄▄▀███▄█
+  █▄▄ ▀▀▄▄▀  █▄ ▄▄▀█▄▀█▄▀█▀▀█▄ ▀▀  ▀██▄▄▀█  ▄▄▀▄▀▄▄▄█
+  ██ ▀▀█▄▄▀█▀█▄  ▄▀█▀▄▀█▀██▄ ▀▄  ▀█▀▄▄ ██▄▄▀▄▄▀█▀█▄▄█
+  █▄▄▄███▄▄▀▀▄▄▀▀▀▀▄▀▄ ▄▀ ▄▄▄ ▄▀█ █▀▀█▀ ▄▄▄ ▄▄▄ ▀▄ ██
+  █ ▄▄▄▄▄ █▄█ █▀▀█▄▄▀ ██▀ █▄█ ▄▀█▀▀▀█▄██▀ █ █▄█ ▄▄▀ █
+  █ █   █ █   ▀▄▀█▄▀▀  ▄█ ▄▄▄▄▄▀▀  █▄▄ ▄ ▄▀  ▄▄ ▀▄▄▄█
+  █ █▄▄▄█ █  ▄▀█▄█▄ ▀▄▄▄█ ▄ ▄▄▄▀ █ ▀▄▄ ▄  ██▄▀▄▀▀██▀█
+  █▄▄▄▄▄▄▄█▄█▄▄███▄▄▄▄▄▄███▄▄▄▄█▄▄███▄█▄▄▄▄█▄█▄██▄▄▄█
+
+
+🍎 Open this link on your iOS devices (or scan the QR code) to install the app:
+https://expo.dev/accounts/cleblanc189/projects/AwesomeProject/builds/922575d4-7572-4abd-8a24-3957df1e638e
+
+@cleblanc-nuance ➜ /workspaces/mix-react-native-mobile (main) $ npx expo start --tunnel --dev-client
+Starting project at /workspaces/mix-react-native-mobile/AwesomeProject
+Starting Metro Bundler
+Tunnel connected.
+Tunnel ready.
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+█ ▄▄▄▄▄ █▄▀ ▀ █ ██  ▄▄██ ▀█ █ █ ▄▄▄▄▄ █
+█ █   █ █   █▀ ▄█ ▀▄▀▄█ ▀▄ ▀▄██ █   █ █
+█ █▄▄▄█ █▄█▀ ▄▄ ▄▀ █ ▀█▄██  ▄ █ █▄▄▄█ █
+█▄▄▄▄▄▄▄█▄█ █ █ █▄▀ ▀ █▄▀ █▄█ █▄▄▄▄▄▄▄█
+█▄  ▄██▄▀▄▀ ▄ █▄██ ▀▀▀ ▀ ██▄▄  ██▄▄▄▀▄█
+██ ▄█▄█▄▄█▄▀▄▄▀▀█▀█▄▀██  █▀▀█▄▄ █▄  ▀██
+█▄█▀▄▀▀▄█▀▄▀▀  ▄▀▄ █ ██ █▀   ▀▀    ▄▀ █
+████▄██▄ ▀▄█ █▄██ ▄█  ▀▄█▄█▄█  ▄█▄▄▄ ██
+████▄▄ ▄▄█▄▀█▄ ▀ ▀▀▄█▀▄▀   ▀▀▄▄▀██▄ ▄ █
+█▄▄▄▀▀█▄  █ ▀██▀▄▄▀██▀▄ █▀█ ▄ ▄▀ ▀▀▄▀▄█
+█ ▀█ ▀▄▄█▄▄███▀█▄█▄ ▀     ▀██ ▄█ ▄█▀▀██
+█ ██▀█ ▄▄ ▀▄▀▀ ██▀▄ █ ▄█  ▀▀██▀█▀▀  ███
+█▄ ▄██ ▄▄▄▄▀ ▄▄ ▀▄ ███▄▄▄▄▄  ▀▄ ▀   █▀█
+█▀▄▄▀ ▄▄ ▄▄▄██▄▄▀█▄▀ █▀ ███▀▀▀█ ▀ ▄▄ ▀█
+████▄█▄▄▄ █   ▄█▀ ▀▄██▄▀  ▄█▄ ▄▄▄ ▀ █▀█
+█ ▄▄▄▄▄ ██ ▀████ ▄▀██▄▄ █ █ █ █▄█ ▀█  █
+█ █   █ █▀▄▀▀██ █ ▄  █   █▀▄█   ▄  ████
+█ █▄▄▄█ █ ▄█▄  ▄▀█▄▀█ ▄▀▄ ▀ ▄▄█▄  ▀▀███
+█▄▄▄▄▄▄▄█▄▄██▄▄▄██▄███▄▄█▄▄▄███▄███▄▄▄█
+
+› Metro waiting on exp+awesomeproject://expo-development-client/?url=http%3A%2F%2Fxwikomc.cleblanc189.8081.exp.direct
+› Scan the QR code above with Expo Go (Android) or the Camera app (iOS)
+
+› Press a │ open Android
+› Press w │ open web
+
+› Press j │ open debugger
+› Press r │ reload app
+› Press m │ toggle menu
+
+› Press ? │ show all commands
+
+Logs for your project will appear below. Press Ctrl+C to exit.
+› Reloading apps
+
+```
+## To Learn more about the native code see the README.md in ./AwesomeProject/react-native-nuance-mix
+### The Android and iOS native code is in the react-native-nuance-mix folder is implemented in Java and Objective-C respectively
+### The components provided by the native plugin react-native-nuance-mix are used in the App.js file in the root of the project. 
